98072893bf1521fcede8933584ea26d142ecbf16
Dakdar <karthik.dakdar@sap.com>
2021-11-09 09:39:42 +0530

    commited by karthik

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ import Profiles from './components/profiles/Profiles';
 import Posts from './components/posts/Posts';
 import Post from './components/post/Post';
 import Profile from './components/profile/Profile';
-//redux
+//redux part for the code
 import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './action/auth';
